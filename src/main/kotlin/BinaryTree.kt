bd20301f1d96578b468d89229ec901bb7cc7494b
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-04-11 09:30:42 +0100

    Refactor class to allow passing the two subtrees to a binary tree

--- src/main/kotlin/BinaryTree.kt
+++ src/main/kotlin/BinaryTree.kt
@@ -5,6 +5,11 @@ import jdk.incubator.vector.VectorOperators.Binary
 class BinaryTree<T>(var value: T) {
     var left: BinaryTree<T>? = null
     var right: BinaryTree<T>? = null
+
+    constructor(value: T, left: BinaryTree<T>?, right: BinaryTree<T>?) : this(value) {
+        this.left = left
+        this.right = right
+    }
 }
 
 fun <T> invertBinaryTree(root: BinaryTree<T>): BinaryTree<T>? {
